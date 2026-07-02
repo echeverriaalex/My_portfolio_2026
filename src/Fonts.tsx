f093c3c988f06190c083a9d89706e852ecec6109
Alex Echeverria <aleex.naahuel@outlook.com>
2026-07-02 00:48:56 -0300

    modifico archivo nombre y codigo

---
+++ src/Fonts.tsx
@@ -0,0 +1,3 @@
+export const ORBITRON = { fontFamily: "'Orbitron', sans-serif" };
+export const EXO = { fontFamily: "'Exo 2', sans-serif" };
+export const MONO = { fontFamily: "'Share Tech Mono', monospace" };
